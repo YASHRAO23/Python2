7474da8ca20d049865878ece1b7e97e02a473a37
YASHRAO23 <yashchinchole11@gmail.com>
2024-12-12 01:37:32 +0530

    Quiz completed

--- marval/Practice 2.py
+++ marval/Practice 2.py
@@ -1,1 +1,64 @@
-jfidh suh
+##### 2D collections ##########
+
+# fruits = {"apple","banana","orange","cetaphal"}
+# vegitables = ["cabages","mushrooms","tomato","batata"]
+# meats =["chicken","fish","tandori","tikka"]
+
+# groceries =[fruits,vegitables,meats]
+
+# for category in groceries:
+#     for fruits in category:
+#         print(fruits, end=" ")
+#     print()
+        
+        
+######### 2D tuple ########
+
+# num_pad = ((1, 2, 3),
+#            (4, 5, 6),
+#            (7, 8, 9),
+#            ("*", 0, "#"))
+
+# for row in num_pad:
+#     for num in row:
+#         print(num, end="")
+#     print()
+        
+        
+############ Quiz game ###########
+
+question =(("Which one is your fav anime? :"),
+           ("which one is your fav  charactor? :"),
+           ("which one power you wish you can have in real life? :"),
+           ("Which one is best anime girl charactor? :"),
+           ("Tell me episod number you want to repeadted daily? :"))
+
+options = (("A.one piece","B.Nauroto","C.Solo leveling","D.jijutsu"),
+           ("A.Kakashi","B.Naruto","C.levi","D.itachi"),
+           ("A.rasingan","B.chakara","C.lighting strike","D.telepoart"),
+           ("A.Henata","B.sakura","C.liya","D.kriya"),
+           ("A.225","B.336","C.55","D.08"))
+
+answers = ("C", "A", "C", "A", "D")
+
+guesses = []
+score = 0
+question_num = 0
+
+
+for question in question:
+    print("-------------------------------")
+    print(question)
+    for option in options[question_num]:
+        
+        print(option)
+        
+    guess = input("Enter (A, B, C, D): ").upper()
+    guesses.append(guess)
+    if guesses == answers[question_num]:
+        score +=1
+        print("CORRECT!")
+    else:
+        print("INCORRECT!")
+        print(f"{answers[question_num]}")
+        question_num += 1
--- marval/practice.py
+++ marval/practice.py
@@ -441,27 +441,27 @@
 
 ############## Shopping cart program ##################
 
-# foods =[]
-# prices=[]
-# total =0
+foods =[]
+prices=[]
+total =0
 
-# while True:
-#     food = input("Enter a food to buy(q to quite) : ")
-#     if food.lower()== "q":
-#         break
-#     else:
-#         price = float(input(f"Enter the price of a {food}: $"))
-#         foods.append(food)
-#         prices.append(price)
+while True:
+    food = input("Enter a food to buy(q to quite) : ")
+    if food.lower()== "q":
+        break
+    else:
+        price = float(input(f"Enter the price of a {food}: $"))
+        foods.append(food)
+        prices.append(price)
 
-# print("-----Your Cart-----")
+print("-----Your Cart-----")
 
-# for food in foods:
-#     print(food, end="")
+for food in foods:
+    print(food, end="")
     
-# for price in prices:
-#     total += price
+for price in prices:
+    total += price
 
-# print()  
-# print(f"YOUR Total is :${total}")
+print()  
+print(f"YOUR Total is :${total}")
 
